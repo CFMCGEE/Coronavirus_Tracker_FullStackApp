dd85008e8cc8c0166f4e3194b6a8a6b4d28c2bf0
CFMCGEE <funchbusiness@gmail.com>
2021-12-13 00:14:30 -0500

    Fixing things still

--- front-end/src/header_and_footer/Header.js
+++ front-end/src/header_and_footer/Header.js
@@ -6,14 +6,14 @@ const HeaderComponent = () => {
 
     return (
   
-      <Navbar className="backgroundTab">
+      <Navbar className={["backgroundTab"]}>
         <COVIDAPILogo style={{ width: "180px", height: "40px" }} />
-      <Container>
+      <Container className="header_width">
       <Nav className={["me-auto", "backgroundTab"]}>
-        <Nav.Link style={{ marginLeft: "27.5rem", color: "#FFFFFF", fontSize: "100%" }} href="https://covidactnow.org/?s=26601560" target="_blank" rel="noreferrer noopener">COVID Data from the Tri-State Area of Delaware, Maryland, and Pennsylvania</Nav.Link>
+        <Nav.Link style={{ marginLeft: "32.5rem", color: "#FFFFFF", fontSize: "100%" }} href="https://covidactnow.org/?s=26601560" target="_blank" rel="noreferrer noopener">COVID Data from the Tri-State Area of Delaware, Maryland, and Pennsylvania</Nav.Link>
       </Nav>
       <Nav>
-          <Nav.Link style={{ marginLeftt: "100px", color: "#FFFFFF", fontSize: "100%" }} href="https://github.com/CFMCGEE/Coronavirus_Tracker_FullStackApp" target="_blank" rel="noreferrer noopener">Source Code?</Nav.Link>
+          <Nav.Link style={{ marginLeftt: "200px", color: "#FFFFFF", fontSize: "100%" }} href="https://github.com/CFMCGEE/Coronavirus_Tracker_FullStackApp" target="_blank" rel="noreferrer noopener">Source Code?</Nav.Link>
       </Nav>
       <Nav>
           <span className="new_tag">new</span>
